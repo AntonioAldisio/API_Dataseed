7a476cb123e36fdb0245e5bf4129a183960da630
c1326986 Antonio Aldisio <c1326986@interno.bb.com.br>
2023-05-30 17:34:03 -0300

    feat: conexao com docker

--- Start.sh
+++ Start.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-yoyo apply -a
-# uvicorn main:app --reload --host 0.0.0.0 --port 80
+
+sleep 15
+
+yoyo apply --all
+
+if [ $? -eq 0 ]; then
+    uvicorn main:app --reload --host 0.0.0.0 --port 80
+else
+    echo "Error: yoyo apply failed. Application startup aborted."
+fi
