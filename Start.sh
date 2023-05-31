c3e8a5ed45491c5441cf17d43fb40df34cf7f665
c1326986 Antonio Aldisio <c1326986@interno.bb.com.br>
2023-05-31 00:43:22 -0300

    inclusao de logs

--- Start.sh
+++ Start.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-
+echo "Irei esperar 15 segundos"
 sleep 15
 
+echo "Realizando migrations"
 yoyo apply --all
 
 if [ $? -eq 0 ]; then
+    echo "Migrations realizada com sucesso"
     uvicorn main:app --reload --host 0.0.0.0 --port 80
 else
     echo "Error: yoyo apply failed. Application startup aborted."
